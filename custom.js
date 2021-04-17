7160f16ad4d37c152bc0105544f6581bd480c901
Yashas Shah <79524299+YashasTheDev@users.noreply.github.com>
2021-04-17 10:56:57 -0400

    Create custom.js

---
+++ custom.js
@@ -0,0 +1,10 @@
+// ID - Club Entry Badge
+/******************************
+* Applied To | YourRealName   *
+* Role       | YourRealRole   *
+* --------------------------- *
+* Replit     | @Username      *
+* GitHub     | @Username      *
+* --------------------------- *
+* Badge Type | ID             *
+******************************/
