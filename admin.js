64d468f583494bd08664c2fa9a9bd8c78090f392
Yashas Shah <79524299+FirewallStampede@users.noreply.github.com>
2021-04-17 10:53:08 -0400

    Create admin.js

---
+++ admin.js
@@ -0,0 +1,9 @@
+/******************************
+* Applied To | Yashas Shah    *
+* Role       | Software Dev   *
+* --------------------------- *
+* Replit     | @YashasShah    *
+* GitHub     | @YashasTheDev  *
+* --------------------------- *
+* Badge Type | ID             *
+******************************/
