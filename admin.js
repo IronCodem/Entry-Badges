1135fc422a0268745f92ece03eac31d2e14b8a32
Yashas Shah <79524299+YashasTheDev@users.noreply.github.com>
2021-04-18 11:33:34 -0400

    Update admin.js

--- admin.js
+++ admin.js
@@ -5,5 +5,17 @@
 * Replit     | @YashasShah    *
 * GitHub     | @YashasTheDev  *
 * --------------------------- *
+* Badge Type | Headmaster     *
+******************************/
+
+/******************************
+* Applied To | Yashas Shah    *
+* Role       | Software Dev   *
+* --------------------------- *
+* Replit     | @YashasShah    *
+* GitHub     | @YashasTheDev  *
+* --------------------------- *
 * Badge Type | ID             *
 ******************************/
+
+// You'll need both to join and they have to be authorized by Admin/Headmaster (same role)
